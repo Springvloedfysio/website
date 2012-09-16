83c51f6490c381a6cc3fb1e3183584d3fd08a112
Jochem Maas <jochem@iamjochem.com>
2012-09-16 22:33:24 +0200

    add old site for archiving (clean out new stuff from archive branch)

--- build/server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect'),
-    http    = require('http'),
-    fs      = require('fs'),
-    app     = connect().use(connect.static(__dirname + '/../public/'))
-            ;
-
-http.createServer(app).listen(3000);
-
-fs.writeFileSync(__dirname + '/server.pid', process.pid, 'utf-8');
